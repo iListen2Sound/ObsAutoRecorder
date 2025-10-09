d33e3d57cee544a414e46b3e775b3a607b104b35
iListen2Sound <75883296+iListen2Sound@users.noreply.github.com>
2025-10-08 20:13:22 -0400

    Create TagHolder.cs file

---
+++ TagHolder.cs
@@ -0,0 +1,151 @@
+﻿using Il2CppPhoton.Realtime;
+using Il2CppRUMBLE;
+using Il2CppRUMBLE.Interactions.InteractionBase;
+using Il2CppRUMBLE.Managers;
+using Il2CppRUMBLE.Social;
+using Il2CppRUMBLE.UI;
+using Il2CppTMPro;
+using JetBrains.Annotations;
+using MelonLoader;
+using OBS_Control_API;
+using System.IO;
+using RumbleModdingAPI;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using UnityEngine.Video;
+using Il2CppSteamworks;
+using System.Threading.Tasks;
+using static OBS_Control_API.RequestResponse;
+
+
+[assembly: MelonInfo(typeof(ObsAutoRecorder.ObsAutoRecorder), ObsAutoRecorder.BuildInfo.Name, ObsAutoRecorder.BuildInfo.Version, ObsAutoRecorder.BuildInfo.Author)]
+[assembly: MelonGame("Buckethead Entertainment", "RUMBLE")]
+[assembly: MelonAuthorColor(255, 87, 166, 80)]
+[assembly: MelonColor(255, 87, 166, 80)]
+
+namespace ObsAutoRecorder
+{
+	public static class BuildInfo
+	{
+		public const string Name = "ObsAutoRecorder";
+		public const string Author = "iListen2Sound";
+		public const string Version = "1.0.0";
+	}
+	public class TagHolder : MelonMod
+	{
+		//ideal location for autorecord status 0.2391 -0.0336 -0.0091
+		//friendblock path --------------LOGIC--------------/Heinhouser products/Telephone 2.0 REDUX special edition/Friend Screen/Player Tags/Player Tag 2.0/InteractionButton/Meshes/
+		//status block location: playertag 0 0 0 
+		public bool WasPressed { get; set; } = false;
+
+		public GameObject RecordIconBlock { get; private set; }
+		public GameObject RecordIcon { get; private set; }
+
+
+		private bool _autoRecordable = false;
+
+		public GameObject InteractionButton
+		{
+			get
+			{
+				return _tagObject.transform.GetChild(0).gameObject;
+			}
+		}
+		/// <summary>
+		/// Gets or sets a value indicating whether automatic recording is enabled.
+		/// </summary>
+		/// <remarks>When set to <see langword="true"/>, the UI updates to reflect the auto-recording status. Changing
+		/// this property may affect the appearance of the record icon.</remarks>
+		public bool AutoRecordable
+		{
+			get { return _autoRecordable; }
+			set
+			{
+				_autoRecordable = value;
+				Color statusColor = _autoRecordable ? new Color(0.45f, 0.31f, 0.22f, 1f) : new Color(0.56f, 0.52f, 0.4f, 1f);
+				RecordIcon.transform.GetChild(0).GetComponent<RawImage>().color = statusColor;
+			}
+		}
+		public string PlayFabID
+		{
+			get
+			{
+				return _tagObject.GetComponent<Il2CppRUMBLE.Social.Phone.PlayerTag>()._UserData_k__BackingField.playFabMasterId;
+			}
+		}
+		public string PublicName
+		{
+			get
+			{
+				return Sanitize(_tagObject.GetComponent<Il2CppRUMBLE.Social.Phone.PlayerTag>()._UserData_k__BackingField.publicName);
+			}
+		}
+		/*public GameObject StatusIcon
+		{
+			get
+			{
+				return _tagObject.transform.GetChild(0).GetChild(1).GetChild(3).GetChild(0).gameObject;
+			}
+		}*/
+		private GameObject _tagObject;
+
+		/// <summary>
+		/// Returns a string that represents the current object, including the PlayFab ID and public name.
+		/// </summary>
+		/// <returns>A string in the format "PlayFabID - PublicName" representing the current object.</returns>
+		public override string ToString()
+		{
+			return $"{PlayFabID} - {PublicName}";
+		}
+
+		public GameObject TagObject
+		{
+			get { return _tagObject; }
+			set
+			{
+				_tagObject = value;
+				ObsAutoRecorder.Instance.Log($"TagObject set for {PublicName}", true);
+				CreateAutoRecordBlock();
+			}
+		}
+
+		private void CreateAutoRecordBlock()
+		{
+			RecordIconBlock = GameObject.Instantiate(TagObject.transform.GetChild(0).GetChild(0).GetChild(0).gameObject);
+
+			RecordIconBlock.transform.SetParent(TagObject.transform.GetChild(0).GetChild(0), false);
+			RecordIconBlock.transform.localPosition = new Vector3(0.2391f, -0.0336f, -0.0091f);
+
+			RecordIcon = ObsAutoRecorder.GetIndicator();
+			RecordIcon.transform.SetParent(RecordIconBlock.transform, false);
+			RecordIcon.SetActive(true);
+			RecordIcon.transform.localPosition = new Vector3(0, 0.5f, 0);
+			//0.0085 0.0085 0.0085
+			RecordIcon.transform.localScale = new Vector3(0.0085f, 0.0085f, 0.0085f);
+			RecordIcon.transform.localRotation = Quaternion.Euler(90, 0, 0);
+			//new Color (R = .45, G = .31, B = .22)
+			AutoRecordable = false;
+
+		}
+
+		public TagHolder()
+		{
+
+
+		}
+
+		public static string Sanitize(string Input)
+		{
+
+			string pattern = @"<[^>]*>";
+			return Regex.Replace(Input, pattern, string.Empty);
+		}
+	}
+}
