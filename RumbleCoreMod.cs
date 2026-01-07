24016e6b01690bbfa6cfab33a92a16859ad873b6
iListen2Sound <75883296+iListen2Sound@users.noreply.github.com>
2026-01-07 09:27:00 -0500

    Updated BuildInfo Version Number

--- RumbleCoreMod.cs
+++ RumbleCoreMod.cs
@@ -36,7 +36,7 @@ namespace ObsAutoRecorder
 	{
 		public const string Name = "ObsAutoRecorder";
 		public const string Author = "iListen2Sound";
-		public const string Version = "1.1.0";
+		public const string Version = "1.2.0";
 	}
 	public partial class ObsAutoRecorder : MelonMod
 	{
